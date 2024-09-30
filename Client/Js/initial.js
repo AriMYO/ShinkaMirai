9dc0ca6f1447bae48f1c8fcf5b5642e6350b3ea4
LALO611adad <166889169+Cris-CM@users.noreply.github.com>
2024-09-30 01:19:35 -0500

    feat: Navegacion entre paginas y agregado boton continuar

--- Client/Js/initial.js
+++ Client/Js/initial.js
@@ -1,18 +1,20 @@
 function verificarRespuesta(button) {
-    // Obtener todas las opciones
-    const botones = document.querySelectorAll("button[name='q1']");
-    
-    // Limpiar todos los botones antes de aplicar estilos
-    botones.forEach(btn => {
-        btn.classList.remove('correcto', 'incorrecto');
-    });
+  // Obtener todas las opciones
+  const botones = document.querySelectorAll("button[name='q1']");
+  
+  // Limpiar todos los botones antes de aplicar estilos
+  botones.forEach(btn => {
+    btn.classList.remove('correcto', 'incorrecto');
+  });
 
-    // Verificar si la respuesta es correcta o incorrecta
-    if (button.value === "correcto") {
-        button.classList.add('correcto');
-        document.getElementById('mensaje1').textContent = "¡Es correcto!";
-    } else {
-        button.classList.add('incorrecto');
-        document.getElementById('mensaje1').textContent = "Respuesta incorrecta.";
-    }
-} 
+  // Verificar si la respuesta es correcta o incorrecta
+  if (button.value === "correcto") {
+    button.classList.add('correcto');
+    document.getElementById('texto-mensaje').textContent = "¡La respuesta es correcta!";
+    document.getElementById('continuar').style.display = 'inline-block';
+  } else {
+    button.classList.add('incorrecto');
+    document.getElementById('texto-mensaje').textContent = "La respuesta es incorrecta.";
+    document.getElementById('continuar').style.display = 'none';
+  }
+}
